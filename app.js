823ffd067801ed96524f1b1dc23111960f381ff7
kwonangela <kwonangelaa@gmail.com>
2022-11-17 21:58:27 -0500

    Prompt 3 calculate - with extra console.log examples

--- app.js
+++ app.js
@@ -22,5 +22,45 @@ function reverseWordOrder(word){
     return reversed;
 }
 
-console.log(reverseWordOrder("Ishmael me Call"));
-console.log(reverseWordOrder("I use Lâncome on my comb"));
+// console.log(reverseWordOrder("Ishmael me Call"));
+// console.log(reverseWordOrder("I use Lâncome on my comb"));
+// ------------
+// ### 3. calculate
+// Write a function called `calculate`.
+// This function should take three arguments: two numbers and a string.
+// Name the parameters `num1`, `num2`, and `operation`.
+// If if the function is called with the third argument as “add”, it should return the sum of num1 and num2.
+// If if the function is called with the third argument as “sub”, it should return return `num1` minus `num2`.
+// Do the same thing for multiplication “mult”, division “div”, and exponent “exp” (where `num2` is the exponent of `num1`).
+// console.log(calculate(4, 3, "sub"));=> 
+// console.log(calculate(4, 3, "exp"));=> 64
+
+function calculate(num1, num2, operation){
+    let num;
+    if (operation === "add"){
+        return num1 + num2;
+    } else if (operation === "sub"){
+            return num1 - num2;
+        } else if (operation === "mult"){
+            return num1 * num2;
+        } else if (operation === "div"){
+            return num1/num2;
+        } else if (operation === "exp"){
+            let i=1;
+            let num=1;
+            while (i<=num2){
+                num=num*num1;
+                i++
+            }
+            return num;
+        }
+}
+
+
+console.log(calculate(4,3,"sub"));
+console.log(calculate(4,3,"exp"));
+console.log(calculate(5,4,"exp"));
+console.log(calculate(6,2,"exp"));
+console.log(calculate(4,3,"add"));
+console.log(calculate(4,3,"mult"));
+console.log(calculate(9,2,"div"));
